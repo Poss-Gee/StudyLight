53f9344e2aea0c81935a1c2caf24a375262cee43
Possi Gee <possigee96@gmail.com>
2025-08-06 15:43:47 +0000

    It's not fisx yet

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,11 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 import "dotenv/config";
 
 export const ai = genkit({
   plugins: [googleAI({
-    apiKey: process.env.GEMINI_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
   })],
   model: 'googleai/gemini-2.0-flash',
 });
