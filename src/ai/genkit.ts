15e354680a8b7211c53c3f8ad44b878cc7130099
Possi Gee <possigee96@gmail.com>
2025-08-08 17:37:16 +0000

    I can't generate the note anymore with ai

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,7 +5,7 @@ import "dotenv/config";
 
 export const ai = genkit({
   plugins: [googleAI({
-    apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+    apiKey: process.env.GEMINI_API_KEY,
   })],
   model: 'googleai/gemini-2.0-flash',
 });
